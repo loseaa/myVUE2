b1282151aec404956c81193c9b51fe7290f566af
nbld <3028942051@qq.com>
2025-07-22 22:58:25 +0800

    diff算法预备

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { initData, initWatch } from './init/state.js';
+import {patch,createEle} from './vdom/index.js'
 
 import { template2Function } from './render/index.js';
 import { mountComponent } from './lifecycle.js';
@@ -20,6 +21,13 @@ lifecycleMixin(Vue)
 renderMixin(Vue)
 globalApi(Vue)
 stateMixin(Vue)
+
+
+
+
+
+
+    
 
 Vue.prototype.$init = function () {
     this.$options = mergeOptions(this.constructor.$options, this.$options);
@@ -60,3 +68,30 @@ Vue.prototype.$mount = function (el) {
 }
 
 
+//测试patch功能
+    let vm1 = new Vue({
+        data(){
+            return{
+                a:1,
+                b:2,
+            }
+        }
+    })
+    let render1=template2Function("<div style='color:red' x='1'>{{a}}</div>")
+    let vnode1=render1.call(vm1)
+
+    document.body.appendChild(createEle(vnode1))
+    
+    let vm2 = new Vue({
+        data(){
+            return{
+                a:1,
+                b:2,
+            }
+        }
+    })
+    let render2=template2Function("<div style='background:blue' class='a' y='2'></div>")
+    let vnode2=render2.call(vm2)
+setTimeout(()=>{
+    patch(vnode1,vnode2)
+},1000)
--- src/vdom/index.js
+++ src/vdom/index.js
@@ -1,13 +1,89 @@
 export function patch(oldVnode, newVnode) {
+    if (oldVnode instanceof HTMLElement) {
+        let newEl = createEle(newVnode);
+        oldVnode.parentNode.replaceChild(newEl, oldVnode);
+        return newEl;
+    }
+    else {
+        //标签不同直接删除更新
+        if (newVnode.tag != oldVnode.tag) {
+            let newEl = createEle(newVnode);
+            oldVnode.el.parentNode.replaceChild(newEl, oldVnode.el);
+            return newEl;
+        }
+        //标签相同如果是文字也直接更新
+        if (newVnode.text) {
+            oldVnode.el.textContent = newVnode.text;
+            return oldVnode.el;
+        }
 
-    let newEl = createEle(newVnode);
-    oldVnode.parentNode.replaceChild(newEl, oldVnode);
-    document.body.appendChild(newEl);
-    return newEl;
+        //这里逻辑是不是文本节点，要复用节点
+        let el=newVnode.el=oldVnode.el;
+        patchAttr(oldVnode, newVnode);
+        patchChildren(oldVnode,newVnode,el);
+        return oldVnode.el;
+    }
 
 }
 
-function createEle(Vnode) {
+function patchAttr(oldVnode, newVnode) {
+    for (let key in oldVnode.attrs) {
+        if (!newVnode.attrs[key]) {
+            oldVnode.el.removeAttribute(key);
+        }
+    }
+    let newAttrs = newVnode.attrs || {};
+    let oldAttrs = oldVnode.attrs || {};
+
+
+    for (let key in newAttrs) {
+        if (key == "style") continue;
+        if (!oldAttrs[key] || oldAttrs[key] != newAttrs[key]) {
+            oldVnode.el.setAttribute(key, newVnode.attrs[key]);
+        }
+    }
+
+    let newStyle = newAttrs.style || {};
+    let oldStyle = oldAttrs.style || {};
+
+    for (let key in oldStyle) {
+        if (!newStyle[key]) {
+            oldVnode.el.style[key] = "";
+        }
+    }
+
+    for (let key in newStyle) {
+        oldVnode.el.style[key] = newStyle[key];
+    }
+
+}
+
+function patchChildren(oldVnode,newVnode,el){
+    let oldChildren=oldVnode.children||[];
+    let newChildren=newVnode.children||[];
+
+    console.log(oldChildren,newChildren);
+    //旧的节点没有儿子，直接添加新的
+    if(oldChildren.length==0){
+        newChildren.forEach(child=>{
+            el.appendChild(createEle(child));
+        })
+        return;
+    }
+    if(newChildren.length==0){
+        el.innerHTML="";
+        return;
+    }
+
+    updateChildren(oldChildren,newChildren,el);
+}
+
+//diff算法的核心
+function updateChildren(oldChildren,newChildren,el){
+
+}
+
+export function createEle(Vnode) {
     let { tag, data, children, key, text } = Vnode;
     let el;
     if (tag) {
